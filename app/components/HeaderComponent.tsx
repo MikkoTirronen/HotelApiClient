87a570ae74b73d08915c84261d4c880ad592ce36
Mikko Tirronen <mikkoatirronen@gmail.com>
2025-11-18 19:16:52 +0100

    added a header component

---
+++ app/components/HeaderComponent.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="w-full text-center py-4 mb-6 bg-blue-600 text-white text-2xl font-bold rounded-xl shadow">
+      Hotel Booking System
+    </div>
+  );
+}
