4ce4eda27cda192f91da711ca0d909eca1550403
Mikko Tirronen <mikkoatirronen@gmail.com>
2025-11-19 13:36:38 +0100

    added routes

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-
+  route("/customers", "routes/customers.tsx"),
+  route("/rooms", "routes/rooms.tsx"),
   route("*", "routes/notfound.tsx"),
 ] satisfies RouteConfig;
