12c7e7ff82547b03658e66ff597cc596ce4f6b8f
Mikko Tirronen <mikkoatirronen@gmail.com>
2025-11-19 14:49:19 +0100

    added vertical nav

---
+++ app/components/MainNavVertical.tsx
@@ -0,0 +1,30 @@
+import { Link, useLocation } from "react-router";
+
+export default function MainNavVertical() {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  const navItems = [
+    { name: "Bookings", path: "/" },
+    { name: "Rooms", path: "/rooms" },
+    { name: "Customers", path: "/customers" },
+  ];
+
+  return (
+    <nav className="flex flex-col space-y-2">
+      {navItems.map((item) => (
+        <Link
+          key={item.name}
+          to={item.path}
+          className={`px-4 py-2 rounded ${
+            currentPath === item.path
+              ? "bg-blue-500 text-white"
+              : "text-gray-700 hover:bg-blue-100"
+          }`}
+        >
+          {item.name}
+        </Link>
+      ))}
+    </nav>
+  );
+}
